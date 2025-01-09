b172ae58618da302b65287fec63020e860a97ce9
SantiagoAmores <santi251998@gmail.com>
2025-01-09 12:06:01 +0100

    correcciones menu necesarias

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,35 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Importar para manejar textos
 
 public class Menu : MonoBehaviour
 {
-    public GameObject menuPrincipal; // Panel del menú principal
-    public GameObject menuCreditos; // Panel del menú créditos
+
+    public GameObject menuPrincipal;
+    public GameObject menuCreditos;
+    public GameObject botonAtras;
+
+    public string dificultad = "Fácil";
+
+    public TextMeshProUGUI textoDificultad;
 
     public AudioSource audioSource;  // Componente AudioSource para reproducir sonidos
     public AudioClip sonidoBoton;    // Sonido que se reproducirá al pulsar botones
 
-    public void IniciarJuego()
+    private void Start()
     {
-        ReproducirSonido(); // Reproducir el sonido al pulsar el botón
-        SceneManager.LoadScene("SampleScene"); // Cargar la escena principal
+        menuPrincipal.SetActive(true);
+        menuCreditos.SetActive(false);
+        botonAtras.SetActive(false);
     }
 
-    public void ReproducirSonido()
+    private void IniciarJuego()
     {
-        // Verifica que el AudioSource y el sonido están configurados
-        if (audioSource != null && sonidoBoton != null)
+
+        //Cargar escena con el juego
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void Creditos()
+    {
+        menuPrincipal.SetActive(false);
+        menuCreditos.SetActive(true);
+        botonAtras.SetActive(true);
+    }
+
+    public void Atras()
+    {
+        menuPrincipal.SetActive(true);
+        menuCreditos.SetActive(false);
+        botonAtras.SetActive(false);
+    }
+
+    public void DificultadJuego(string nivel) 
+    {
+        dificultad = nivel;
+        
+        if (textoDificultad != null )
         {
-            audioSource.PlayOneShot(sonidoBoton); // Reproduce el sonido
+            textoDificultad.text = dificultad;
         }
     }
 
-    void Start()
-    {
-        // Configurar los objetos al inicio del juego
-        menuPrincipal.SetActive(true);   // Mostrar menú principal
-        menuCreditos.SetActive(false);  // Ocultar menú créditos
-    }
 }
